60a06c14b5770d2f31d7ed8a2bc14c29d0d1b6f7
Francisco Ríos <frios@deloitte.es>
2021-10-22 15:41:40 +0200

    docs: :memo: TODO: Handle login errors

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,19 @@
 import axios, {
   setAuthorizationHeader,
 } from './axiosClient';
+import storage from '../../utils/storage';
 
-export const login = async (username, password) => {
-  const accessToken = await axios.post(`http://localhost:8000/auth/login`, username, password);
+export const login = async (user, pass) => {
+  const accessToken = await axios.post(`http://localhost:8000/auth/login`, {user, pass});
   setAuthorizationHeader(accessToken);
-  localStorage.set('token', accessToken);
+  storage.set('token', accessToken);
   return accessToken;
+  
+  /* TODO: Error management --> try-catch section 
+    try {
+      Axios request
+    }
+    catch (error) {Managment error. No saving token in localStorage and no setAuthorizationHeader}
+  */
 };
 
