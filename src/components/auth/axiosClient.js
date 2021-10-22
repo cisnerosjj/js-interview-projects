d503aa50d76682d46a83f0eac0228e36a3430557
Francisco Ríos <frios@deloitte.es>
2021-10-22 15:38:10 +0200

    refactor: :recycle: Configure axios client for handling errors and retrieve only {data} property

--- src/components/auth/axiosClient.js
+++ src/components/auth/axiosClient.js
@@ -4,6 +4,19 @@ const axiosClient = axios.create({
   baseURL: 'http://localhost:8000'
 });
 
+axiosClient.interceptors.response.use(
+  response => response.data,
+  error => {
+    if (!error.response) {
+      return Promise.reject({ message: error.message });
+    }
+    return Promise.reject({
+      message: error.response.statusText,
+      ...error.response.data,
+    });
+  },
+);
+
 export const setAuthorizationHeader = token => {
   const { accessToken } = token;
   axiosClient.defaults.headers.common[
