e9b0f2c8873f452d55dba92c58545038c2f99ec8
cisnerosjj <jorgecisnerosgavin@gmail.com>
2021-10-22 13:22:08 +0200

    add login-register form

--- src/components/Home/PopUpLogin.jsx
+++ src/components/Home/PopUpLogin.jsx
@@ -1,53 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../Home/PopUpLogin.css";
 
 const PopUpLogin = ({ setShowPopUp }) => {
   const [nickname, setNickname] = useState();
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const toggleSignup = () => {
+    document.getElementById("login-toggle").style.backgroundColor = "#fff";
+    document.getElementById("login-toggle").style.color = "#222";
+    document.getElementById("signup-toggle").style.backgroundColor = "#57b846";
+    document.getElementById("signup-toggle").style.color = "#fff";
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+  };
+
+  const toggleLogin = () => {
+    document.getElementById("login-toggle").style.backgroundColor = "#57B846";
+    document.getElementById("login-toggle").style.color = "#fff";
+    document.getElementById("signup-toggle").style.backgroundColor = "#fff";
+    document.getElementById("signup-toggle").style.color = "#222";
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+  };
+  useEffect(() => {
+    toggleLogin();
+    toggleSignup();
+  }, []);
+
   return (
     <div className="modalBackground">
-      <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button
-            onClick={() => {
-              setShowPopUp(false);
-            }}
-          >
-            X
-          </button>
-        </div>
-        <div className="title">
-          <h3>Enter your name to continue</h3>
-        </div>
-        <div className="body">
-          <input
-            className="ui input"
-            type="text"
-            placeholder="Enter your Name"
-            value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
-          ></input>
-        </div>
-        <div className="footer">
-          <button
-            className="ui danger button"
-            onClick={() => {
-              setShowPopUp(false);
-            }}
-            id="cancelBtn"
-          >
-            Cancel
-          </button>
-          <button
-            id="continueBtn"
-            className="ui primary button"
-            type="submit"
-            onClick={() => {
-              setShowPopUp(false);
-            }}
-          >
-            Continue
-          </button>
+      <div className="container">
+        <div className="form-modal">
+          <div className="titleCloseBtn">
+            <button
+              onClick={() => {
+                setShowPopUp(false);
+              }}
+            >
+              X
+            </button>
+          </div>
+          <div className="form-toggle">
+            {
+              <button id="login-toggle" onClick={toggleLogin}>
+                log in
+              </button>
+            }
+            <button id="signup-toggle" onClick={toggleSignup}>
+              Register
+            </button>
+          </div>
+
+          <div id="login-form">
+            <form>
+              <input type="text" placeholder="Enter username" />
+              <input type="password" placeholder="Enter password" />
+              <button type="button" className="btn login">
+                login
+              </button>
+            </form>
+          </div>
+
+          <div id="signup-form">
+            <form>
+              {/* <input type="email" placeholder="Enter your email" /> */}
+              <input type="text" placeholder="Choose username" />
+              <input type="password" placeholder="Create password" />
+              <button type="button" className="btn signup">
+                <i className="fa fa-spinner fa-pulse"></i> create account
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
