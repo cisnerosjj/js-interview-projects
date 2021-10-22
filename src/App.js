c413302e3ccbeb6d3a06d095f48aa4af1c141197
Francisco Ríos <frios@deloitte.es>
2021-10-22 13:55:58 +0200

    feat(AUTH): :sparkles: Add login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Nav from "./components/Home/Nav";
 import Profile from "./components/DisplayInfo/Profile";
+import Login from "./pages/LoginPage";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Nav></Nav>
         <Route exact path="/" component={Home} />
         <Route exact path="/profile" component={Profile}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        {/* TODO: Register page? */}
       </div>
     </Router>
   );
