4fed16658811fc6be6e6109ddd43a7a00d1c85ec
cisnerosjj <jorgecisnerosgavin@gmail.com>
2021-10-29 10:13:19 +0100

    cleaning debuggers

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,13 +1,11 @@
 import axios from "./axiosClient";
 
 export const register = async (username, password) => {
-  debugger;
   try {
     const newUser = await axios.post(`http://localhost:8000/auth/register`, {
       username,
       password,
     });
-    debugger;
     return newUser;
   } catch (err) {
     console.log(err);
